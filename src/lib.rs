658297fc4d39d3d63a1cd0775bd75b05cdbbaaba
Antoine Mazarguil <antoine.mazarguil@gmail.com>
2023-03-13 06:15:57 -0700

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -12,24 +12,25 @@ pub mod coloring;
 pub struct GraphKey(Vec<usize>);
 
 impl GraphKey {
+    pub fn get_descriptor(&self) -> &Vec<usize> {
+        return &self.0
+    }
+}
+
+impl GraphKey {
     pub fn new(g : &Graph::<usize, (), Undirected>) -> GraphKey {
 
-        // 1. Generate first colouring & first refine
+        // Generate first colouring & first refine.
         let mut gc = Colouring::new(g);
         gc.refine(g);
 
-        // 1.1 If gc is discrete, compute key
+        // If gc is discrete, compute the associated key.
         if gc.is_discrete() {
-            println!("Already leaf !");
             let descr = gc.compute_graph_from_discrete(g);
             return GraphKey(compute_descriptor(&descr));
         }
 
-
-        //
-        // 2. Create tree root
-        //
-
+        // Otherwise, set up the tree for exploration.
         let root = {
 
             let target = gc.select_cell_v1();
@@ -51,39 +52,13 @@ impl GraphKey {
         //      * Follows the exploration path of Traces
         //
 
-        //
-        // Vars definition
-        //
-        
         let mut next_list = Vec::from([root]);      // list of colourings to study on next level
-
-        // let mut niter = 0;
-
         let mut leaf_found = false;
-
-        //
-        // Leaf storages
-        //
 
         // let mut leaves_colouing : Vec<Graph<usize, ()>> = Vec::new();
         // let mut leaves_descriptors : Vec<Vec<usize>> = Vec::new();
 
-        //
-        //
-        //
-
         while !leaf_found { 
-
-            /* 
-            println!();
-            println!();
-            println!();
-            println!("//////////////////////////////");
-            println!("//////// Iteration {niter} //////");
-            println!("//////////////////////////////");
-            niter += 1;
-            println!("next_list.len() = {}", next_list.len());
-            */
 
             let current_list = next_list;
             next_list = Vec::new();
@@ -92,65 +67,37 @@ impl GraphKey {
 
             for node in current_list.into_iter() {
 
-                /* 
-                println!("Working on a new Tree node !");
-                println!("Its colouring is :");
-                node.c.print_cells();
-                println!();
-                */
+                // node.c.print_cells();
 
                 let mut node = node;
 
-                //
-                // 3.1 Add son in exploration to next_list (losing ownership)
-                //
-
+                // Add son in exploration to next_list (losing ownership)
                 if let Some(b) = node.son_in_exp_path {
-
                     let k_dim = node.son_k_dim.as_ref().unwrap();
-
                     if b.c.is_discrete() { leaf_found = true; }
-
                     if best_k_dim <= *k_dim { 
-                        
                         if best_k_dim < *k_dim {
                             next_list = Vec::new();
                             best_k_dim = k_dim.clone();
-                        } 
-                        
+                        }
                         next_list.push(*b);
-                        
                     }
-
                     node.son_in_exp_path = None;             
                 }
 
-                //
-                // 3.2 breath search
-                //
-                
                 while node.children.len() > 0 {
 
-                    //
                     // Create new TreeNode from the individualization of a (graph) node from the target cell
-                    //
-
-                    let _v = node.children.pop().unwrap();                       // Pop a random child to individualize
-                    let mut _gc = node.c.clone();                            // Clone the colouring (required since it can be edited in each child of a node)
-                    let new_color = _gc.individualize(node.target_cell, _v);             // Individualize the selected cell
-                    let mut trace = _gc.refine(&g);                         // Refine & generate trace in the process
+                    let _v = node.children.pop().unwrap();
+                    let mut _gc = node.c.clone();
+                    let new_color = _gc.individualize(node.target_cell, _v);
+                    let mut trace = _gc.refine(&g);
                     trace.insert(0, new_color);
-                    let mut k_dim = Kdim::new(_gc.get_cell_count(), trace);       // Generate Kdim object
-                    let mut ancestor_in_exp_path = &mut node;            // parent of the current node "experimental_path_node", created in loop
-                                                                                        // at each iteration, the ownership of the current node is given to the parent
+                    let mut k_dim = Kdim::new(_gc.get_cell_count(), trace);
 
-                    // println!("This node child {} (indiv = {}) has a Kdim {:?}", child_idx, _v, k_dim);
-                    // child_idx += 1;
-
-                    //
-                    // Check if current node is k_dim better
-                    //
-
+                    // at each iteration, the ownership of the current node is given to the parent
+                    let mut ancestor_in_exp_path = &mut node;
+                    
                     if best_k_dim > k_dim {
                         continue;
                     }
@@ -160,19 +107,12 @@ impl GraphKey {
                         best_k_dim = k_dim.clone();
                     }
 
-                    //
-                    // 3.2.1 Compute experimental path
-                    //
-
+                    // Compute experimental path
                     loop {
                         
                         if _gc.is_discrete() {
-
-                            // 1. Compute associated graph
-                            // 2. Compute associated Kdim
                             
                             // TODO : check automorphisms
-
 
                             let leaf = TreeNode{ 
                                 c : _gc, 
@@ -189,7 +129,7 @@ impl GraphKey {
                         
                         let target = _gc.select_cell_v1();
                         let mut children = _gc.get_cell_members(target);
-                        children.sort_by(|a, b| b.cmp(a));                                                      // TODO : delete
+                        children.sort_by(|a, b| b.cmp(a));             // TODO : delete
                         let mut new_experimental_path_node = TreeNode{ 
                             c : _gc, target_cell: 
                             target, children : children, 
@@ -204,15 +144,10 @@ impl GraphKey {
                         trace.insert(0, new_color);
                         k_dim = Kdim::new(_gc.get_cell_count(), trace);
 
-                        // Give ownership of the new node to its parent & create a new &mut 
-
+                        // Give ownership of the new node to its parent & create a new &mut
                         ancestor_in_exp_path.son_in_exp_path = Some(Box::new(new_experimental_path_node));
                         ancestor_in_exp_path = ancestor_in_exp_path.son_in_exp_path.as_deref_mut().unwrap();
                     }
-                
-                    //
-                    // 3.2.2 
-                    //
                     
                     if let Some(_n) = node.son_in_exp_path {
                         if _n.c.is_discrete() { leaf_found = true; }
@@ -221,11 +156,6 @@ impl GraphKey {
                     }
                 }
             }
-
-            if next_list.len() == 0 {
-                panic!("Should not occur !")
-            }
-
         }
 
         // println!("Final leaf count = {}", next_list.len());
@@ -255,20 +185,7 @@ struct TreeNode {
     son_k_dim : Option<Kdim>,
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-pub fn compute_descriptor(g : &Graph<usize, (), Undirected>) -> Vec<usize> {
+fn compute_descriptor(g : &Graph<usize, (), Undirected>) -> Vec<usize> {
     let n = g.node_count();
     let mut cononical = vec![n];
     let mut prev_neigh;
@@ -291,17 +208,141 @@ pub fn compute_descriptor(g : &Graph<usize, (), Undirected>) -> Vec<usize> {
 //
 //
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    use petgraph::graph::{UnGraph, NodeIndex};
+    use rand::{Rng, thread_rng};
+    use rand::seq::SliceRandom;
+    use std::collections::HashSet;
+    use petgraph::algo::is_isomorphic;
+
+    fn gen_test_graph() -> Graph::<usize, (), Undirected> {
+    
+        let edges : Vec<(u32, u32)> = vec![
+            (0, 3), (0, 5), (0, 8), (1, 4), (1, 6), (1, 8),
+            (2, 5), (2, 7), (3, 6), (3, 9), (4, 7), (4, 9),
+            (5, 8), (7, 9)
+        ];
+    
+        return UnGraph::from_edges(edges);
+    }
+
+    
+    fn generate_random_graph(n : usize, p : f64) -> Graph::<usize, (), Undirected> {
+        
+        let mut rng = rand::thread_rng();
+        let mut g = UnGraph::<usize, ()>::new_undirected();
+        g.reserve_nodes(n);
+        (0..n).for_each(|i| { g.add_node(i); });
+        
+        for i in 0..n {
+            for j in (i+1)..n {
+                if rng.gen_range((0.)..(1.)) < p {
+                    g.add_edge(NodeIndex::new(i), NodeIndex::new(j), ());
+                }
+            }
+        }
+
+        return g
+    }
+
+    
+    fn generate_permutated_graph(g : &Graph::<usize, (), Undirected>) -> Graph::<usize, (), Undirected> {
+
+        let n = g.node_count();
+        let mut perm : Vec<usize> = (0..n).collect();
+        let mut rng = thread_rng();
+        perm.shuffle(&mut rng);
+
+        
+        let edges : Vec<(usize, usize)> = g.edge_indices()
+        .map(|e| { 
+            let (u, v) = g.edge_endpoints(e).unwrap();
+            (perm[u.index()] , perm[v.index()])
+        })
+        .collect();
+
+        let mut g = UnGraph::<usize, ()>::new_undirected();
+
+        g.reserve_nodes(n);
+        (0..n).for_each(|_| { g.add_node(1); });
+
+        g.reserve_edges(edges.len());
+        edges.into_iter().for_each(|(u, v)| { g.add_edge(NodeIndex::new(u), NodeIndex::new(v), ()); });
+
+        return g
+    }
+
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn key_generation() {
+        
+        let g1 = gen_test_graph();
+        let g2 = generate_permutated_graph(&g1);
+        
+        let key1 = GraphKey::new(&g1);
+        let key2 = GraphKey::new(&g2);
+        
+        assert_eq!(key1, key2);
     }
+
+    #[test]
+    fn key_generation_large() {
+        
+        let g1 = generate_random_graph(2000, 0.05);
+        let g2 = generate_permutated_graph(&g1);
+        
+        let key1 = GraphKey::new(&g1);
+        let key2 = GraphKey::new(&g2);
+        
+        assert_eq!(key1, key2);
+    }
+
+    #[test]
+    fn hashset_graphkeys() {
+        
+        let mut g = generate_random_graph(1000, 0.1);
+        
+        let g1 = generate_permutated_graph(&g);
+        let g2 = generate_permutated_graph(&g);
+        
+        match g.find_edge(0.into(), 1.into()) {
+            Some(_ix) => { g.remove_edge(_ix); }
+            None => { g.add_edge(0.into(), 1.into(), ()); }
+        }
+        
+        let g3 = generate_permutated_graph(&g);
+        let g4 = generate_permutated_graph(&g);
+
+        // generate Hashset
+        let mut s = HashSet::new();
+
+        s.insert(GraphKey::new(&g1));
+        s.insert(GraphKey::new(&g2));
+        s.insert(GraphKey::new(&g3));
+        s.insert(GraphKey::new(&g4));
+
+        assert_eq!(s.len(), 2);
+    }
+
+
+    #[test]
+    fn is_isomorphic_test() {
+
+        for _ in 0..100 {
+            let g1 = generate_random_graph(500, 0.05);
+            let g2 = generate_random_graph(500, 0.05);
+            let g3 = generate_permutated_graph(&g1);
+
+            let key1 = GraphKey::new(&g1);
+            let key2 = GraphKey::new(&g2);
+            let key3 = GraphKey::new(&g3);
+
+            assert_eq!(is_isomorphic(&g1, &g2), key1 == key2);
+            assert_eq!(key1, key3);
+        }
+    }
+
+
 }
--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,23 @@ use petgraph::algo::is_isomorphic;
 use graphkey::GraphKey;
 use std::time::Instant;
 
+use std::collections::HashSet;
+
 fn main() {
 
     // let g1 = gen_test_graph();
-    let g1 = generate_random_graph(100, 0.1);
+    let g1 = generate_random_graph(3000, 0.1);
     let g2 = generate_permutated_graph(&g1);
 
     let start = Instant::now();
     let key1 = GraphKey::new(&g1);
-    let key2 = GraphKey::new(&g1);
+    let key2 = GraphKey::new(&g2);
     let are_isomorphic_graphkey = key1 == key2;
     let duration_graphkey = start.elapsed();
+
+    let start = Instant::now();
+    let _ = key1 == key2;
+    let duration_check = start.elapsed();
 
     let start = Instant::now();
     let are_isomorphic_petgraph = is_isomorphic(&g1, &g2);
@@ -25,8 +31,26 @@ fn main() {
 
     println!("Isomorphis check with petgraph : {} ({:?})", are_isomorphic_petgraph, duration_petgraph);
     println!("Isomorphis check with graphkey : {} ({:?})", are_isomorphic_graphkey, duration_graphkey);
+    println!("Check of key1 == key2 ({:?})", duration_check);
+    println!("Len(key) = {}", key1.get_descriptor().len());
+
+    let g1 = generate_random_graph(1000, 0.1);
+    let g2 = generate_random_graph(1000, 0.1);
+    let g3 = generate_permutated_graph(&g1);
+    let g4 = generate_permutated_graph(&g2);
+
+    let mut m = HashSet::new();
+
+    m.insert(GraphKey::new(&g1));
+    m.insert(GraphKey::new(&g2));
+    m.insert(GraphKey::new(&g3));
+    m.insert(GraphKey::new(&g4));
+
+    println!("m.len() = {}", m.len());
+
 }
 
+#[allow(dead_code)]
 fn gen_test_graph() -> Graph::<usize, (), Undirected> {
     
     let edges : Vec<(u32, u32)> = vec![
@@ -40,8 +64,6 @@ fn gen_test_graph() -> Graph::<usize, (), Undirected> {
 
     return UnGraph::from_edges(edges);
 }
-
-
 
 fn generate_random_graph(n : usize, p : f64) -> Graph::<usize, (), Undirected> {
 
